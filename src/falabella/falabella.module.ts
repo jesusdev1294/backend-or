bced4e34cc09226e7f9b9d0b29fbf0074f0b7e39
Jesus Fernandez <fernandezblancojesus@gmail.com>
2026-01-07 23:27:35 -0300

    complete flow v2

--- src/falabella/falabella.module.ts
+++ src/falabella/falabella.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { FalabellaService } from './falabella.service';
 import { FalabellaController } from './falabella.controller';
 import { LogsModule } from '../logs/logs.module';
+import { OdooModule } from '../odoo/odoo.module';
 import { BullModule } from '@nestjs/bull';
 
 @Module({
   imports: [
     LogsModule,
+    OdooModule,
     BullModule.registerQueue({
       name: 'stock-updates',
     }),
