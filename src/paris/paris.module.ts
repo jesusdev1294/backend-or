f27071732e4fdf03ab3b4fe542cd188aa54c57bd
richardggarcia <syoliverts@gmail.com>
2026-01-22 09:13:25 -0300

    fix: remove duplicate BullModule.registerQueue from Ripley and Paris modules

--- src/paris/paris.module.ts
+++ src/paris/paris.module.ts
@@ -3,15 +3,11 @@ import { ParisService } from './paris.service';
 import { ParisController } from './paris.controller';
 import { LogsModule } from '../logs/logs.module';
 import { OdooModule } from '../odoo/odoo.module';
-import { BullModule } from '@nestjs/bull';
 
 @Module({
     imports: [
         LogsModule,
         OdooModule,
-        BullModule.registerQueue({
-            name: 'stock-updates',
-        }),
     ],
     controllers: [ParisController],
     providers: [ParisService],
